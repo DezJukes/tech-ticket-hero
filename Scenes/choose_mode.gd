602fd8215fa38e159257b59185a9757b897c26ae
Manduriaga Emman <121656643+IEMDomain04@users.noreply.github.com>
2026-03-30 17:00:15 +0800

    Revised Main Menu; Draft Beginner Level;
    Added furniture assets; Redesign Main Menu; Redesign Beginner Level;

--- Scenes/choose_mode.gd
+++ Scenes/choose_mode.gd
@@ -1,10 +1,7 @@
 extends Node
 
-
-
-func _on_story_mode_pressed():
-	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
-
-
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _on_the_office_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,4 +1,6 @@
 extends Node
 
-func _on_start_game_pressed():
+
+
+func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/choose_mode.tscn")
