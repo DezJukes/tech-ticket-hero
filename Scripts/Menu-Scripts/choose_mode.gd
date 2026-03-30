9c557079c4be0d2a1c65069a722559717eb2fa9e
Manduriaga Emman <121656643+IEMDomain04@users.noreply.github.com>
2026-03-30 21:35:22 +0800

    Organized Folder Structure for the Menu
    Just created a folder for the scenes and scripts for us to be more organize

--- Scripts/Menu-Scripts/choose_mode.gd
+++ Scripts/Menu-Scripts/choose_mode.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
 
 func _on_the_office_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
