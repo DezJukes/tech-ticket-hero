c7abef98fc8f305ddf055f94a521acecb943b79c
Manduriaga Emman <121656643+IEMDomain04@users.noreply.github.com>
2026-04-14 11:13:30 +0800

    TODO: Design each stages of the game;

--- Scenes/Modals/success_modal.gd
+++ Scenes/Modals/success_modal.gd
@@ -13,4 +13,4 @@ func _process(delta: float) -> void:
 
 func _on_mission_complete_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Scenes/Level 1 - Beginner/Beginner - Architecture/Library_game_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level 1 - Beginner/Beginner - Game Scene/Library_game_scene.tscn")
