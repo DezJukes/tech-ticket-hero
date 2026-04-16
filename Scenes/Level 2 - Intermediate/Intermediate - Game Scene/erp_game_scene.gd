2eef98304d156a6556be4e8770f5b8bf4f56846d
AugustinBrain <austinbrain25@gmail.com>
2026-04-17 02:50:01 +0800

    Added scene for intermediate

---
+++ Scenes/Level 2 - Intermediate/Intermediate - Game Scene/erp_game_scene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var pause_panel = %PausePanel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_interact_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Level 2 - Intermediate/Intermediate - Architecture/ERP_Architecture.tscn")
+
+
+func _on_pause_button_pressed() -> void:
+	get_tree().paused = true
+	pause_panel.show()
