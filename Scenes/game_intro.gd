d6226c3c595df9f274fb23c9cfcb26a06d16a6b8
DezJukes <jbryan08dc@gmail.com>
2026-05-03 21:11:55 +0800

    Added animation to Level 1 and Level 2
    Changes:
    - fade in to black on enter of scene level 1 and level 2
    - fade in text on scene level 1 and level 2
    - fade out text on scene level 1 and level 2

---
+++ Scenes/game_intro.gd
@@ -0,0 +1,27 @@
+extends Node
+@onready var black_screen = $TITLE/Control/ColorRect
+@onready var intro_text = $TITLE/Control/VBoxContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play_intro()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func play_intro() -> void:
+	black_screen.modulate.a = 1.0
+	intro_text.modulate.a = 0.0
+
+	var tween = create_tween()
+
+	tween.tween_property(intro_text, "modulate:a", 1.0, 1.0)
+	tween.tween_interval(1.5)
+	tween.tween_property(intro_text, "modulate:a", 0.0, 1.0)
+	tween.tween_property(black_screen, "modulate:a", 0.0, 1.5)
+	
+	await tween.finished
+	
+	$TITLE.queue_free()
